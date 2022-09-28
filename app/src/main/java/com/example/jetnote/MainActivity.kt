9b0c49ce5a94f9c5c85720579247e88f10a49d29
joey sparidaans <jsparidaans@gmail.com>
2022-09-28 11:40:52 +0200

    created NoteRow and showing dummy data

--- app/src/main/java/com/example/jetnote/MainActivity.kt
+++ app/src/main/java/com/example/jetnote/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetnote.data.NoteDataSource
 import com.example.jetnote.screen.NoteScreen
 import com.example.jetnote.ui.theme.JetNoteTheme
 
@@ -23,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     NoteScreen(
-                        notes = emptyList(),
+                        notes = NoteDataSource().loadNotes(),
                         onAddNote = {},
                         onRemoveNote = {},
                     )
--- app/src/main/java/com/example/jetnote/screen/NoteScreen.kt
+++ app/src/main/java/com/example/jetnote/screen/NoteScreen.kt
@@ -1,16 +1,19 @@
 package com.example.jetnote.screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,7 +21,9 @@ import androidx.compose.ui.unit.dp
 import com.example.jetnote.R
 import com.example.jetnote.components.NoteButton
 import com.example.jetnote.components.NoteInputText
+import com.example.jetnote.data.NoteDataSource
 import com.example.jetnote.model.Note
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun NoteScreen(
@@ -74,14 +79,58 @@ fun NoteScreen(
                 }
             })
         }
+
+        Divider(modifier = Modifier.padding(10.dp))
+
+        LazyColumn {
+            items(notes) { note ->
+                NoteRow(note = note, onNoteClicked = {})
+            }
+        }
     }
+}
+
+@Composable
+fun NoteRow(
+    modifier: Modifier = Modifier,
+    note: Note,
+    onNoteClicked: (Note) -> Unit,
+) {
+    Surface(
+        modifier = modifier
+            .padding(4.dp)
+            .clip(RoundedCornerShape(topEnd = 33.dp, bottomStart = 33.dp))
+            .fillMaxWidth(),
+        color = Color(0xFFDFE6EB),
+        elevation = 6.dp
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 14.dp, vertical = 6.dp)
+                .clickable { }, horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                text = note.title,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Text(
+                text = note.description,
+                style = MaterialTheme.typography.subtitle1
+            )
+            Text(
+                text = note.entryDate.format(DateTimeFormatter.ofPattern("EEE, d, MMM")),
+                style = MaterialTheme.typography.caption
+            )
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun NoteScreenPreview() {
     NoteScreen(
-        notes = emptyList(),
+        notes = NoteDataSource().loadNotes(),
         onAddNote = {},
         onRemoveNote = {},
     )
